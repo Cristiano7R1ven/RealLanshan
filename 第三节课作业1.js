34c1811c5ffeb6937be4f76f16395988bcc51404
SpikeCN <60869797+SpikeCN@users.noreply.github.com>
2023-07-29 19:55:55 +0800

    Update 第三节课作业1.js

--- 第三节课作业1.js
+++ 第三节课作业1.js
@@ -29,6 +29,22 @@ function easiestSolving(arr)
 let result = arr.flat(Infinity);
 console.log(result); // [1, 2, 3, 4, 5, 6, 7, 8]
 }
+function flatten(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]));//连接两个数组
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+let arr = [1, [2, [3, [4, 5], 6], 7], 8];
+console.log(flatten(arr)); // [1, 2, 3, 4, 5, 6, 7, 8]
 
 const arr = [1, [2, [3, 4], 5], 6];
 console.log(flattenRecursive(arr)); // [1, 2, 3, 4, 5, 6]
