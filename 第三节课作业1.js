eda2e580345e4ddc4b7e98e5648b1759809ef7d4
SpikeCN <60869797+SpikeCN@users.noreply.github.com>
2023-07-29 19:44:12 +0800

    Create 第三节课作业1.js

---
+++ 第三节课作业1.js
@@ -0,0 +1,35 @@
+function flattenRecursive(arr) {
+  const result = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      result.push(...flattenRecursive(item));
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+function flattenIterative(arr) {
+  const result = [];
+  const stack = [...arr];
+  while (stack.length) {
+    const item = stack.pop();
+    if (Array.isArray(item)) {
+      stack.push(...item);
+    } else {
+      result.unshift(item);
+    }
+  }
+  return result;
+}
+function easiestSolving(arr)
+{
+
+let result = arr.flat(Infinity);
+console.log(result); // [1, 2, 3, 4, 5, 6, 7, 8]
+}
+
+const arr = [1, [2, [3, 4], 5], 6];
+console.log(flattenRecursive(arr)); // [1, 2, 3, 4, 5, 6]
+console.log(flattenIterative(arr)); // [1, 2, 3, 4, 5, 6]
